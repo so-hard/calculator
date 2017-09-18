45650da77cbdeaf70e5fbce2cb0f4a2e01fc6ef8
so-hard <qq1136339397@gmail.com>
2017-09-18 19:43:00 +0800

    重新构造了 js 代码

--- js/index.js
+++ js/index.js
@@ -62,6 +62,7 @@
 
 // 2.0
 // 获取key里面的按钮所有的按钮 数字 小数点和 功能按钮 将其保存在 btns 中
+
 var isKeys= document.getElementsByClassName("keys")[0];
 var  btns =isKeys. getElementsByTagName("span");
 // 获取屏幕对象的文本节点
@@ -70,70 +71,81 @@ var isArr = [Screen.innerHTML ];
 var clearBtn = document.getElementsByClassName("clear")[0];
 var backspaceBtn = document.getElementsByClassName("backspace")[0];
 for (let i = 0; i < btns.length; i++) {
-  btns[i].onclick = function ( ) {
-    console.log(isArr);
-    //当按钮是数字
-    if (!isNaN(this.innerHTML)) {
-      //如果第一次点击的是0 的话 不做任何操作将 0 放入数组中 然后删除队列的一个元素
-      if (this.innerHTML== 0 && (isArr.length == 1&&isArr[0] ==0)) {
-        isArr.push(this.innerHTML);
-        isArr.shift();
-      }
-      //如果第一次点击的数字按钮 用按钮的文本节点替换Screen的文本节点 将按钮的文本节点压入数组 然后删除队列的第一个元素
-      // 否则 就进行拼接 然候将文本节点进行拼接;
-      else if((isArr.length<=1)&&(isArr[0]=="0")) {
-        Screen.innerHTML=this.innerHTML;
-        isArr.push(this.innerHTML);
-        isArr.shift();
-      }
-      else {
-        Screen.innerHTML+=this.innerHTML;
-        isArr.push(this.innerHTML);
-      } // 1. 当点击的是小数点时候 如果屏幕中只有0的话直接进行拼接 然后将小数点压入数组  2.小数点的拼接只能在前一位置是数字的时候
-    }else if ( this.innerHTML == ".") {
-      if (isArr.length == 1 && isArr[0] == "0") {
-        Screen.innerHTML += this.innerHTML;
-        isArr.push(this.innerHTML);
-      }else if (!isNaN(isArr[isArr.length-1])){
-        isArr.push(this.innerHTML);
-        Screen.innerHTML += this.innerHTML;
-      } // 当点击的是 = 号的时候 调用 eval 方法进行运算 然后清空数组 调用stringToAr 将结果转化成数组
-    }else if (this.innerHTML == "=") {
-      Screen.innerHTML=eval(Screen.innerHTML);
-      isArr.length = 0;
-      stringToArr(Screen.innerHTML , isArr );
-      console.log(isArr);
-    }else { //剩下的就是按钮就是运算符 当运算符点击前的元素是数字且
-      if (!isNaN(isArr[isArr.length-1]) && (isArr.length>=1 && isArr[0] != 0)) {
-        Screen.innerHTML += this.innerHTML;
-        isArr.push(this.innerHTML);
-      }
-    }
-  }
-  }
+	btns[i].onclick = function ( ) {
+
+		//isNum ( );
+		// console.log(isArr);
+		//当按钮是数字
+		if (!isNaN(this.innerHTML)) {
+			//如果第一次点击的是0 的话 不做任何操作将 0 放入数组中 然后删除队列的一个元素
+			if (this.innerHTML== 0 && (isArr.length == 1&&isArr[0] ==0)) {
+				isArr.push(this.innerHTML);
+				isArr.shift();
+			}
+			//如果第一次点击的数字按钮 用按钮的文本节点替换Screen的文本节点 将按钮的文本节点压入数组 然后删除队列的第一个元素
+			// 否则 就进行拼接 然候将文本节点进行拼接;
+			else if((isArr.length<=1)&&(isArr[0]=="0")) {
+				Screen.innerHTML=this.innerHTML;
+				isArr.push(this.innerHTML);
+				isArr.shift();
+			}
+			else {
+				Screen.innerHTML+=this.innerHTML;
+				isArr.push(this.innerHTML);
+			} // 1. 当点击的是小数点时候 如果屏幕中只有0的话直接进行拼接 然后将小数点压入数组  2.小数点的拼接只能在前一位置是数字的时候
+		} else if ( this.innerHTML == ".") {
+			if (isArr.length == 1 && isArr[0] == "0") {
+				Screen.innerHTML += this.innerHTML;
+				isArr.push(this.innerHTML);
+			}else if (!isNaN(isArr[isArr.length-1])){
+				isArr.push(this.innerHTML);
+				Screen.innerHTML += this.innerHTML;
+			} // 当点击的是 = 号的时候 调用 eval 方法进行运算 然后清空数组 调用stringToAr 将结果转化成数组
+		}else if (this.innerHTML == "=") {
+			Screen.innerHTML=eval(Screen.innerHTML);
+			isArr.length = 0;
+			stringToArr(Screen.innerHTML , isArr );
+			// console.log(isArr);
+		}else { //剩下的就是按钮就是运算符 当运算符点击前的元素是数字且
+			if (!isNaN(isArr[isArr.length-1]) && (isArr.length>=1 && isArr[0] != 0)) {
+				Screen.innerHTML += this.innerHTML;
+				isArr.push(this.innerHTML);
+			}
+		}
+			isOv (Screen) ;
+	};
+}
 
 clearBtn.onclick = doClear;
 backspaceBtn .onclick = doBackspace;
+
 function doClear ( ) {
-  isArr.length = 0;
-  Screen.innerHTML = 0;
-  isArr.push(Screen.innerHTML);
+	isArr.length = 0;
+	Screen.innerHTML = 0;
+	isArr.push(Screen.innerHTML);
 }
 
 function doBackspace () {
-    console.log(isArr);
-  if (isArr.length == 1) {
-    Screen.innerHTML = 0;
-    isArr[0] = 0;
-  }else {
-      Screen.innerHTML=Screen.innerHTML.slice(0,-1);
-      isArr.pop();
-  }
+	// console.log(isArr);
+	if (isArr.length == 1) {
+		Screen.innerHTML = 0;
+		isArr[0] = 0;
+	}else {
+		Screen.innerHTML=Screen.innerHTML.slice(0,-1);
+		isArr.pop();
+	}
 }
 
 function stringToArr (theString , theArr ) {
-  for (var i = 0; i < theString.length; i++) {
-    theArr.push(theString[i]);
-  }
-  return theArr;
+	for (var i = 0; i < theString.length; i++) {
+		theArr.push(theString[i]);
+	}
+	return theArr;
+}
+
+
+function isOv(one, ) {
+	if (one.clientWidth >200) {
+		doBackspace ();
+	}
 }
---
+++ js/indexTwo.js
@@ -0,0 +1,130 @@
+//3.0 运用对象
+
+function getsome(x ) {
+    console.log(x.element.length);
+    switch (x.element.length > 1) { // 判断传进的 文本节点的长度
+        case true:
+        for (let i = 0; i < x.element.length; i++) {
+            x.element[i].onclick = x.done ; // 循环 触发点击事件
+        }
+            break;
+        default:
+        x.element[0].onclick = x.done;
+    }
+};
+
+// 数字按钮的方法
+function  numFunction (y ) {
+    switch (y.innerHTML) {
+        case 0 :
+        //如果第一次点击的是0 的话 不做任何操作
+            if (y.innerHTML== 0 && (isArr.length == 1&&isArr[0] ==0)) {
+                break;
+            }
+            showNumAndSave ( ) ;
+            break;
+            // 1. 当点击的是小数点时候 如果屏幕中只有0的话直接进行拼接 然后将小数点压入数组
+            //2.小数点的拼接只能在前一位置是数字的时候
+            case ".":
+            if (isArr.length == 1 && isArr[0] == "0") {
+                showNumAndSave ( y) ;
+            }else if (!isNaN(isArr[isArr.length-1])){
+                showNumAndSave ( y) ;
+            }
+            break;
+            default:
+            if((isArr.length<=1)&&(isArr[0]=="0")) {
+                Screen.innerHTML=y.innerHTML;
+                isArr.push(y.innerHTML);
+                isArr.shift();
+            }else {
+                showNumAndSave (y);
+            }
+        }
+  };
+
+//将按钮的数字与屏幕显示的数字相加放在屏幕上...
+function  showNumAndSave( y) {
+    Screen.innerHTML  += y.innerHTML;
+    if ( Screen.clientWidth > 200) {// 判断输入的数字长度是否超出长度
+        console.log(1);
+        key.backspaceBtn.done( );
+        alert('is overflow');
+	}
+    isArr.push(y.innerHTML);
+};
+
+var Screen = document.getElementsByClassName("screen")[0]; // 获取屏幕对象
+var isArr = [Screen.innerHTML]; // 建立一个数组用来实时储存 屏幕的数字
+var isKeys= document.getElementsByClassName("keys")[0];  // 获取数字和运算符按钮
+var num =  isKeys.getElementsByClassName('but'); // 获取数字按钮文本节点对象
+var operator = isKeys.getElementsByClassName('operator'); // 获取运算符文本节点对象
+var equal = isKeys.getElementsByClassName('equal'); // 获取 等于文本节点对象
+var clear = document.getElementsByClassName('clear'); // 获取 清楚文本对象对象
+var backspace = document.getElementsByClassName('backspace');  // 获取回撤文本节点对象
+
+//建立一个  按键对象
+var key = {
+    numBtn :{
+        element : num,
+        done : function () {  // 能不能 函数名 来代替匿名函数 作为 方法 ..  2222 怎么精简
+            let y = this;
+             numFunction (y );
+            }
+    },
+    operatorBtn : {
+      element : operator,
+      done : function ( ) {
+        let y = this;
+        if (!isNaN(isArr[isArr.length-1]) && (isArr.length>=1 && isArr[0] != 0)) {
+            showNumAndSave(y);
+        }
+      }
+    },
+    equalBtn : {
+      element : equal,
+      done : function ( ) {
+        Screen.innerHTML=eval(Screen.innerHTML);
+  			isArr.length = 0;
+        for (var i = 0; i < Screen.innerHTML.length; i++) {
+      		isArr.push( Screen.innerHTML[i]);
+      }
+    }
+  },
+    clearBtn : {
+        element : clear,
+        done : function  ( ) {
+        	isArr.length = 0;
+        	Screen.innerHTML = 0;
+        	isArr.push(Screen.innerHTML);
+        }
+},
+    backspaceBtn : {
+        element : backspace,
+        done : function ( ) {
+            if (isArr.length == 1) {
+        		Screen.innerHTML = 0;
+        		isArr[0] = 0;
+        	}else {
+        		Screen.innerHTML=Screen.innerHTML.slice(0,-1);
+        		isArr.pop();
+        	}
+        }
+    }
+};
+
+    getsome (key.numBtn);
+    getsome(key.operatorBtn);
+    getsome(key.equalBtn);
+    getsome(key.clearBtn);
+    getsome(key.backspaceBtn);
+console.log(key.numBtn.element);
+// for (var x in key) {
+// console.log(x);
+// }
+//
+//
+//
+// console.log(Object.keys(key));
+//
+// console.log(Object.getOwnPropertyNames(key));
